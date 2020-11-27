589bad9318880b54a089ff699c81f57df166b2ed
Siwoo Baek <whitelatte46@gmail.com>
2020-11-27 17:26:07 +0900

    add files to make build up initial react-dev-environment

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">별헤는밤 App.js 입니다.</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/library/ViewPhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ViewPhoto() {
+  return (
+    <div className="App">
+      <header className="App-header">뷰포토 컴포넌트 입니다.</header>
+    </div>
+  );
+}
+
+export default ViewPhoto;
