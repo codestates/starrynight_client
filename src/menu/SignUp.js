2a309bf102d1c66664ba2ae2649711df6f82c9ee
Jaehoon Lee <john@Jaehoonui-MacBookPro.local>
2020-12-02 08:29:44 +0900

    :bug: SignUp 사용자의 입력행위와 동시에 실시간 에러메세지 송출 ver.1

--- src/menu/SignUp.js
+++ src/menu/SignUp.js
@@ -39,6 +39,8 @@ class SignUp extends React.Component {
 
   // 정보입력 이벤트
   // State 할당: 입력받은 정보를 위의 빈 스트링으로 셋팅이 된 state값을 채운다.
+
+  //! 콜백을 사용하는 이유: 입력값 한글자라도 그 존재여부에 따라 에러메세지 바로 송출하기 위함.
   handleInputValue = (key) => (text) => {
     // console.log("잘입력되요?")
     // console.log("key", key)
@@ -46,10 +48,64 @@ class SignUp extends React.Component {
     this.setState({
       [key]: text.target.value
     });
+
+    // 에러메세지의 state값을 업데이트하고 아래 렌더부분에서 렌더시킨다.
+    // 에러메세지 - email
+    //! !this.state.email.length를 사용하면 사용자의 입력행위로 length가 1이상이 되었음에도 불구하고 0으로 인식하여 에러메세지를 출력. 따라서 ""를 사용. 
+    //! 또한 다음 input을 채우는 행위를 하면 직전에 작성한 input 쪽에서 작성한 정보가 말그대로 ""가 아니기에 에러메세지를 그제서야 출력 따라서 & !length 를 사용.
+    if (this.state.email !== "" && !this.state.email.length) {
+      return this.setState({
+        errMsgOfEmailBlanks: "이메일은 필수입니다."
+      })
+    }
+    else {
+      this.setState({
+        errMsgOfEmailBlanks: ""
+      })
+    }
+
+    // 에러메세지 - nickname
+    if (!this.state.nickname.length) {
+      return this.setState({
+        errMsgOfNickNameBlanks: "별명을 입력해주세요."
+      })
+    }
+    else {
+      this.setState({
+        errMsgOfNickNameBlanks: ""
+      })
+    }
+
+    // 에러메세지 - password
+    if (!this.state.password.length) {
+      return this.setState({
+        errMsgOfPasswordBlanks: "비밀번호는 필수입니다."
+      })
+    }
+    else {
+      this.setState({
+        errMsgOfPasswordBlanks: ""
+      })
+    }
+
+    // 에러메세지 - mobile
+    if (!this.state.mobile.length) {
+      return this.setState({
+        errMsgOfMobileBlanks: "연락처를 입력해주세요."
+      })
+    }
+    else {
+      this.setState({
+        errMsgOfMobileBlanks: ""
+      })
+    }
+
   };
   // 위의 입력 이벤트로 인해 새로운 정보로 할당된 state값을 활용한다.
   // 회원가입한 신규 유저 정보를 데이터베이스에 저장하도록 셋팅한다.
   // 서버에 회원가입을 요청을 한 후, 로그인 페이지로 요청한다.
+  //! 에러메세지 셋팅을 여기서 또 한 이유 : 모달을 활성화시킨 후 사용자의 입력행위가 없다면 에러메세지 출력할 필요가 없으니, 에러메세지는 당연히 출력이 안되어 있을 것.
+  //! 따라서 아무 입력행위 없이 회원가입버튼을 누르면 모든 정보를 입력하라는 에러메세지를 출력하기 위함.
   handleClickAddNewUserInfo = () => {
     const NewUserInfo = {
       email: this.state.email,
@@ -58,7 +114,7 @@ class SignUp extends React.Component {
       mobile: this.state.mobile
     }
 
-    // 에러메세지의 state값을 초기화하고 아래 렌더부분에서 렌더시킨다.
+    // 에러메세지의 state값을 업데이트하고 아래 렌더부분에서 렌더시킨다.
     // 에러메세지 - email
     if (!this.state.email.length) {
       this.setState({
