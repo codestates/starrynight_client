bac20d38affbfe1717da6fa834549f05f0af9195
Siwoo Baek <whitelatte46@gmail.com>
2020-12-09 11:51:39 +0900

    :memo: 공유버튼 기능 개발(중)
    addphoto -> savephoto 비동기처리 작업 중

--- src/AddPhoto.js
+++ src/AddPhoto.js
@@ -11,10 +11,15 @@ const { Title } = Typography;
 
 // AddPhoto 컴포넌트 진또배기
 function AddPhoto(props) {
+  console.log("AddPhoto에 전달된 히스토리:", props.history);
+  console.log("AddPhoto에 전달된 로컬스토리지:", props.localStorage);
+
   // 로그인 유저 식별정보를 가져온다
-  const user = window.sessionStorage;
+  const userToken = props.localStorage.responseMsg;
+  console.log("AdDPhoto에서 확인된 토큰:", userToken);
 
   // modal에서 관리할 상태들을 설정한다
+  const [PhotoFormData, setPhotoFormData] = useState([]);
   const [PhotoTitle, setPhotoTitle] = useState("");
   const [PhotoLocation, setPhotoLocation] = useState("");
   const [PhotoHashtag, setPhotoHashtag] = useState("");
@@ -29,51 +34,90 @@ function AddPhoto(props) {
     let formData = new FormData();
     formData.append("file", files[0]);
     console.log("업로드 중...", files);
+    setPhotoFormData(formData);
 
-    // header에 실어서 보낼 데이터타입 설정하여 서버에 업로드
-    const config = {
-      header: { "content-type": "multipart/form-data" },
-    };
-    Axios.post(
-      "https://api.mystar-story.com/addphoto",
-      // "http://localhost:8000/addphoto",
-      formData,
-      config
-    ).then((res) => {
-      if (res.data.success) {
-        console.log("업로드완료!", res.data);
-        setFilePath(res.data.url);
-      } else {
-        alert("업로드에 실패했습니다");
-      }
-    });
+    // // 업로드 파일을 drop하면 FormData 태그 인스턴스 생성 후, "file" 속성을 삽입
+    // let formData = new FormData();
+    // formData.append("file", files[0]);
+    // console.log("업로드 중...", files);
+
+    // // header에 실어서 보낼 데이터타입 설정하여 서버에 업로드
+    // const config = {
+    //   header: { "content-type": "multipart/form-data" },
+    // };
+    // Axios.post(
+    //   // "https://api.mystar-story.com/addphoto",
+    //   "http://localhost:8000/addphoto",
+    //   formData,
+    //   config
+    // ).then((res) => {
+    //   if (res.data.success) {
+    //     console.log("업로드완료!", res.data);
+    //     setFilePath(res.data.url);
+    //   } else {
+    //     alert("업로드에 실패했습니다");
+    //   }
+    // });
   };
 
   // 사진업로드 & 서버전송을 정의한다
   const onSubmit = (e) => {
     e.preventDefault(); // 새로고침 방지
 
-    const photoUploadInfo = {
-      userid: user.userId, // from sessionStorage
+    // header에 실어서 보낼 데이터타입 설정하여 서버에 업로드
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    Axios.post(
+      // "https://api.mystar-story.com/addphoto",
+      "http://localhost:8000/addphoto",
+      PhotoFormData,
+      config
+    ).then((res) => {
+      if (res.data.success) {
+        console.log("사진 Dropdown 성공 !!!", res.data);
+        setFilePath(res.data.url);
+      } else {
+        alert("사진 Dropdown 실패");
+      }
+    });
+
+    const photo = {
+      userToken: userToken, // from localStorage
       title: PhotoTitle,
-      hashtag: PhotoHashtag,
       photoPath: FilePath,
       location: PhotoLocation,
     };
 
-    Axios.post("https://api.mystar-story.com/savephoto", photoUploadInfo)
-      // Axios.post("http://localhost:8000/savephoto", photoUploadInfo)
-      .then((res) => {
-        if (res.data.success) {
-          console.log(res.data);
-          message.success("사진 업로드가 완료되었습니다");
-          setTimeout(() => {
-            props.history.push("/");
-          }, 2000);
-        } else {
-          alert("사진 업로드에 실패하였습니다");
+    const hashtag = {
+      hashtag: PhotoHashtag,
+      photoPath: FilePath,
+    };
+
+    // Axios.post("https://api.mystar-story.com/savephoto", photo)
+    Axios.post("http://localhost:8000/savephoto", photo).then((res) => {
+      if (res.data.success) {
+        console.log(res.data);
+        message.success("사진 업로드 성공 !!!... 2초 후 메인으로 이동");
+
+        setTimeout(() => {
+          props.history.push("/");
+        }, 2000);
+
+        // 해시태그가 있으면 해당 정보도 POST한다
+        if (PhotoHashtag !== "") {
+          Axios.post("http://localhost:8000/hashtag", hashtag).then((res) => {
+            if (res.data.success) {
+              console.log(res.data);
+            } else {
+              alert("해시태그 등록 실패");
+            }
+          });
         }
-      });
+      } else {
+        alert("사진 업로드 실패");
+      }
+    });
   };
 
   // 입력필드를 활성화한다(사진제목)
