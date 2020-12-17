80506493d96eed85f6a742c5325a1e2681864a8f
이재훈 <jami8291215@gmail.com>
2020-12-18 04:45:27 +0900

    :art: mypage프로필사진 변경 구현(완)

--- src/menu/Mypage.js
+++ src/menu/Mypage.js
@@ -6,8 +6,12 @@ import Google from "./social_user/Google";
 import Kakao from "./social_user/Kakao";
 
 // css
-import "../css/Mypage.scss"
+import "../css/Mypage.scss";
+import Button from "./Button";
+import { BiCamera } from "react-icons/bi";
 
+
+// const FormData = require('form-data');
 
 
 class Mypage extends React.Component {
@@ -24,6 +28,7 @@ class Mypage extends React.Component {
         profile: this.props.currentUserInfo.profilePath
       },
 
+      previewURL: "",   // 프로필 사진 업로드 전 미리보기를 위한 로컬의 사진 경로
       // currentUserInfo: this.props.currentUserInfo,
 
       // 변경될 유저 정보, 깃북수정요청 전까지는 올바른 변수명으로 전달하기 위해 이렇게 놔두자.
@@ -34,15 +39,76 @@ class Mypage extends React.Component {
       oauth: "",
       profile: "",
 
+
+      isModifyBtnProfile: false,
+
       isModifyBtnOfPw: false,
       isModifyBtnOfNickname: false,
       isModifyBtnOfMobile: false,
 
       isGoogleModalOpen: false,
-      isKakaoModalOpen: false
+      isKakaoModalOpen: false,
+
+      errMsg: ""
     }
     // this.getUserInfo()
     console.log("Mypage STATE PROPS", this.state.currentUserInfo)
+  }
+
+
+  // 프로필 사진 변경
+
+  handleModifyBtnOfProfileClick = () => {
+    this.setState({
+      isModifyBtnProfile: !this.state.isModifyBtnProfile
+    })
+  }
+
+  handleModifyProfileOnChange = (e) => {  // 업로드전 미리보기 & 제출될 사진 state로 저장
+    // e.preventDefault();
+    let reader = new FileReader()
+    let file = e.target.files[0]
+    this.setState({
+      profile: e.target.files[0]
+    })
+    reader.onloadend = () => {
+      this.setState({
+        previewURL: reader.result
+      })
+    }
+    reader.readAsDataURL(file)
+  }
+  handlePost = (e) => {       // 변경 버튼에 클릭이벤트로 걸어놓기.
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", this.state.profile);
+
+    // console.log("formData", this.state.file.File)
+
+    // const photo = window.sessionStorage.getItem("profile")
+    const config = {
+      // headers: formData.getHeaders()
+      // headers: { "Content-type": "imageFile.type" }
+      headers: { 'Content-Type': 'multipart/form-data' }
+      // headers: { 'Content-Type': 'application/json' }
+    };
+
+    axios.patch("https://api.mystar-story.com/user/modify/profile", formData, config)
+      .then((respoense) => {
+        console.log("사진이 업로드 되었습니다.", respoense.data)
+        this.setState({
+          profile: respoense.data.profilePath
+        })
+      })
+      .catch((error) => {
+
+        console.log("사진 업로드 실패", error.response.data)
+        this.setState({
+          errMsg: error.response.data
+        })
+      })
+
   }
 
 
@@ -108,6 +174,12 @@ class Mypage extends React.Component {
         })
         this.handleModifyBtnOfPwClick()
       })
+  }
+  //UX: input에 작성완료하면 엔터로 바로 제출가능하게 함
+  modifyPwOnkeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.requestModifyPw()
+    }
   }
 
   handleModifyBtnOfNicknameClick = () => {
@@ -183,6 +255,7 @@ class Mypage extends React.Component {
   componentDidMount() {
     // if (this.props.isMypageModalOpen === true) {
     //   console.log("마이페이지 기본정보 들어옴?")
+    // this.handlePost()
     this.getUserInfo()
     // }
     // this.props.mypageClick
@@ -191,8 +264,22 @@ class Mypage extends React.Component {
     //   currentUserInfo: this.props.currentUserInfo
     // })
   }
+  // componentDidUpdate() {
+  //   this.handlePost()
+  // }
+
 
   render() {
+
+    // console.log("바뀌어야할 프로필 사진", this.state.currentUserInfo.profile)
+    // let profile_preview = <img className="profile_img" src="https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927" alt="프로필 사진 허허 안나오네" />;
+    // if (this.state.currentUserInfo.profile !== undefined || this.state.currentUserInfo.profile !== "" || !this.state.currentUserInfo.profile.length) {
+    //   profile_preview = <img className="profile_img" src={this.state.profile} alt="프로필 사진" />
+    // }
+    // else {
+    //   profile_preview
+    // }
+
 
     console.log("변경될 비밀번호", this.state.password)
     console.log("변경될 별명", this.state.nickname)
@@ -228,11 +315,64 @@ class Mypage extends React.Component {
 
                   {/* -------------------------- 프로필 사진 업로드 칸 -------------------------*/}
 
-                  <div className="profile_img_box">
-                    <img
+                  <form>
+                    <div className="profile_img_box" onClick={this.handleModifyBtnOfProfileClick}>
+                      <label>
+                        <div className="profile_img_box_1">
+                          <div className="profile_img_box_2">
+                            <BiCamera
+                              className="modify_profile"
+                            />
+                            {/* <img
                       className="profile_img"
-                      src={this.props.currentUserInfo.profile} alt="프로필 사진" />
-                  </div>
+                      src={this.props.currentUserInfo.profile} alt="프로필 사진" /> */}
+                          </div>
+                        </div>
+
+
+                        {
+                          this.state.currentUserInfo.profile !== undefined ?
+                            this.state.profile !== "" ?
+                              <img className="profile_img" src={this.state.previewURL} alt="프로필 사진" />
+                              :
+                              <img className="profile_img" src={this.state.currentUserInfo.profile} alt="프로필 사진" />
+                            :
+                            <img className="profile_img" src="https://t1.daumcdn.net/cfile/tistory/2513B53E55DB206927" alt="프로필 사진 허허 안나오네" />
+                        }
+
+                        {/* {profile_preview} */}
+                        <input type="file"
+                          accept="image/jpg,image/png,image/jpeg,image/gif"
+                          id="modify_profile"
+                          name="modify_profile"
+                          onChange={this.handleModifyProfileOnChange}
+                        />
+                      </label>
+                      <div>{this.state.errMsg}</div>
+                    </div>
+
+                    {/* 프로필 사진 눌렀을 때 변경버튼 나오게 하고 업로드 후 변경버튼을 눌러 submit 했을 땐, 다시 버튼 사라지게 하기. */}
+                    {/* 블린값 반전시켜주는 이벤트는 프로필 사진 클릭아벤트로 걸어두기 */}
+
+                    {this.state.isModifyBtnProfile === true ?
+                      <Button
+
+                        size="small_mypage_profile"
+                        nanoWidth_mypage_profile
+                        color="gray"
+                        onClick={this.handlePost}
+                      >
+                        변경
+                      </Button>
+                      :
+                      null
+
+                    }
+
+
+                  </form>
+
+
                   <h1>{this.state.currentUserInfo.nickname}</h1>
                   <div id="sub">{this.props.currentUserInfo.email}</div>
                   {/* -------------------------- 연락처 입력 칸 -------------------------*/}
@@ -250,7 +390,7 @@ class Mypage extends React.Component {
                     </div>
 
                     <div className="pw_div_inMypage">
-                      <div>
+                      <div className="pw_input_container">
                         <span>비밀번호</span>
 
 
@@ -258,13 +398,15 @@ class Mypage extends React.Component {
                         {this.state.isModifyBtnOfPw === false ?
                           <span>******</span>
                           :
-                          <input type="password" onChange={this.handleChangeUserInfo("password")} />
+                          <span>
+                            <input type="password" onChange={this.handleChangeUserInfo("password")} onKeyPress={this.modifyPwOnkeyPress} />
+                          </span>
                         }
                       </div>
                       {this.state.isModifyBtnOfPw ?
-                        <button onClick={this.requestModifyPw}>저장</button>
+                        <Button onClick={this.requestModifyPw} outline nanoWidth>저장</Button>
                         :
-                        <button onClick={this.handleModifyBtnOfPwClick}>수정</button>
+                        <Button onClick={this.handleModifyBtnOfPwClick} outline nanoWidth color="gray">수정</Button>
                       }
                     </div>
 
@@ -280,9 +422,9 @@ class Mypage extends React.Component {
                         }
                       </div>
                       {this.state.isModifyBtnOfNickname ?
-                        <button onClick={this.requestModifyNickname}>저장</button>
+                        <Button onClick={this.requestModifyNickname} outline nanoWidth>저장</Button>
                         :
-                        <button onClick={this.handleModifyBtnOfNicknameClick}>수정</button>
+                        <Button onClick={this.handleModifyBtnOfNicknameClick} outline nanoWidth color="gray">수정</Button>
                       }
                     </div>
 
@@ -298,9 +440,9 @@ class Mypage extends React.Component {
                         }
                       </div>
                       {this.state.isModifyBtnOfMobile ?
-                        <button onClick={this.requestModifyMobile}>저장</button>
+                        <Button onClick={this.requestModifyMobile} outline nanoWidth color="red">저장</Button>
                         :
-                        <button onClick={this.handleModifyBtnOfMobileClick}>수정</button>
+                        <Button onClick={this.handleModifyBtnOfMobileClick} outline nanoWidth color="gray">수정</Button>
                       }
                     </div>
 
@@ -309,12 +451,12 @@ class Mypage extends React.Component {
 
                   {/* -------------------------- 회원 탈퇴 버튼 칸 --------------------*/}
                   <div>
-                    <div className="button_container_InSignUp">
+                    <div className="Button_container_InSignUp">
                       <div>
                         {/* axios post 요청 보내기 */}
-                        <button onClick={this.props.DoubleCheckRemoveUsersClick} >
+                        <Button onClick={this.props.DoubleCheckRemoveUsersClick} outline nanoWidth>
                           회원 탈퇴
-                    </button>
+                        </Button>
                       </div>
 
                     </div>
